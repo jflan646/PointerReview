55e9aafef6501b4f7f893b2fa7706df565bd7a39
thorveblenred <thorveblenred@gmail.com>
2023-10-29 10:39:48 -0400

    here is he base code for the pointer review

---
+++ PointerReview/rewrite.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int doSomething(int& x, int& y);
+int doSomething2(int* x, int* y);
+int main()
+{
+	int x = 1; 
+	int y = 1;
+	cout << doSomething(x, y) << endl;
+	x = 1;
+	y = 1;
+	cout << doSomething2(&x, &y) << endl;;
+
+}
+/*what is done here*/
+int doSomething(int& x, int& y) {
+	int temp = x;
+	x = y + 10;
+	y = temp * 10;
+	return x + y;
+}
+int doSomething2(int* x, int* y) {
+	int temp = *x;
+	*x = *y + 10;
+	*y = temp * 10;
+	return (*x + *y);
+}
